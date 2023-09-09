f4ef8ac3089769cb2075383560b84b6082e8aeae
InsanityGamer <seanmm1997@gmail.com>
2023-09-09 10:41:20 -0500

    added script

---
+++ install-config.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#Author: InsanityGamer
+#Purpose: Installs my config files and packages automatically from tty1
+#Usage: ./install-config.sh
+#Instruction: use git pull inside of your user home directory
+cd ~/Insanitys-I3-Config-files/ ;
+cp -R ./.config ~/$USER/ ;
+sudo dnf install Xorg lightdm slick-greeter i3 alacritty picom polybar rofi -y ;
+sudo systemctl set-default graphical.target ;
+reboot ;
+
