ac28edb6d04da1e6d9dce868f37b032d4450ab5a
InsanityGamer <seanmm1997@gmail.com>
2023-09-14 20:10:28 -0500

    i forgot qterminal in the dnflist

--- install-config.sh
+++ install-config.sh
@@ -3,9 +3,10 @@
 #Purpose: Installs my config files and packages automatically from tty1
 #Usage: ./install-config.sh
 #Instruction: use git pull inside of your user home directory
-cd ~/Insanitys-I3-Config-files/ ;
+cd $HOME/Insanitys-I3-Config-files/ ;
 cp -R ./.config $HOME/ ;
 cat ./dnflist3 | xargs sudo dnf install ;
-sleep 10; sudo systemctl set-default graphical.target ;
+; sudo systemctl set-default graphical.target ;
+sleep 10
 reboot ;
 
